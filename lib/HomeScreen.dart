6893cb2819d02f2e874ab1af4f5f9ee1ee040f85
NN <72925897+NN3054@users.noreply.github.com>
2021-02-26 09:01:45 +0100

    Neues Design HomeScreen ohne funktionierende buttons

---
+++ lib/HomeScreen.dart
@@ -0,0 +1,30 @@
+
+import 'package:flutter/material.dart';
+
+
+class OnBoarding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        fontFamily: 'avenir'
+      ),
+      home: onboarding(),
+
+    );
+  }
+}
+
+class onboarding extends StatefulWidget {
+  @override
+  _onboardingState createState() => _onboardingState();
+}
+
+class _onboardingState extends State<onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
